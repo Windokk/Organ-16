a705ac9636847151e6e21f0ef42f79fbc5b51457
Windokk <68743762+Windokk@users.noreply.github.com>
2025-08-25 18:46:06 +0200

    Added graphics system (full framebuffer)

--- programs/draw_red_rect.py
+++ programs/draw_red_rect.py
@@ -2,6 +2,6 @@ import compiler
 
 if __name__ == "__main__":
     segments = [
-        ("draw_red_rect.pglu", 0x0000)
+        ("draw_red_rect.org", 0x0000)
     ]
     compiler.CompileMultiple(segments, "draw.bin")
--- programs/tests.py
+++ programs/tests.py
@@ -2,7 +2,7 @@ import compiler
 
 if __name__ == "__tests__":
     segments = [
-        ("first_test.pglu", 0x0000),
-        ("second_test.pglu", 0x3000),
+        ("first_test.org", 0x0000),
+        ("second_test.org", 0x3000),
     ]
     compiler.CompileMultiple(segments, "out.bin")
