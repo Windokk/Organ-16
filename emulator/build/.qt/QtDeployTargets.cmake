ab8cc940d9d021875bea26d7cac5b9d073d0fb24
Windokk <68743762+Windokk@users.noreply.github.com>
2025-09-14 23:46:06 +0200

    Fix

--- emulator/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Emulator_FILE C:/Users/elipt/Desktop/Organ-16/emulator/build/Emulator.exe)
-set(__QT_DEPLOY_TARGET_Emulator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Emulator_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
--- emulator/build/CMakeFiles/Emulator.dir/Emulator_autogen/UVLADIE3JM/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Emulator.dir/Emulator_autogen/UVLADIE3JM/qrc_resources.cpp.obj: \
- C:\Users\elipt\Desktop\Organ-16\emulator\build\Emulator_autogen\UVLADIE3JM\qrc_resources.cpp
